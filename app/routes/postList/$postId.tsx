57201fbf20499f50c7005fb80ad46534119bd51c
gabehug <ghug@luc.edu>
2022-12-05 22:09:35 -0600

    initializing file structure

---
+++ app/routes/postList/$postId.tsx
@@ -0,0 +1,10 @@
+export default function PostRoute() {
+  return (
+    <div>
+      <p>This is a post apparently:</p>
+      <p>
+        Example of what pulling specific posts would pull after getting database setup.
+      </p>
+    </div>
+  );
+}

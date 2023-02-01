c65c40af2a5037fac0b496c6cf9909e06fa82c36
gabehug <ghug@luc.edu>
2023-02-01 13:07:05 -0600

    added script to root.tsx for google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -34,8 +34,8 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export const loader = async () => {
-  return json({ gaTrackingId: process.env.GOOGLE_ID });
+export const loader = () => {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
 };
 
 export default function App() {
@@ -57,6 +57,30 @@ export default function App() {
         <LiveReload />
       </head>
       <body>
+      {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
+        
         <Outlet />
       </body>
     </html>
